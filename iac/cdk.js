41d9472da7064c25e7959b890510c51064e027f6
JP Dillingham <jp@dillingham.me>
2023-03-05 12:17:01 -0600

    use fs for file manipulation

--- iac/cdk.js
+++ iac/cdk.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-new */
 /* eslint-disable import/no-extraneous-dependencies */
 
+const fs = require('fs');
 const { execSync } = require('child_process');
 const cdk = require('aws-cdk-lib');
 const { LambdaCdkStack } = require('./stack');
@@ -11,9 +12,9 @@ const { LambdaCdkStack } = require('./stack');
 // we'll use npm to create the structure and omit any dev dependencies
 // note that we'll need to refer to the '.layer' directory in the stack config
 console.log('⏳  Updating layer dependencies...\n');
-execSync('mkdir -p ./.layer/nodejs');
-execSync('cp package.json ./.layer/nodejs/package.json');
-execSync('cp package-lock.json ./.layer/nodejs/package-lock.json');
+fs.mkdirSync('./.layer/nodejs', { recursive: true });
+fs.copyFileSync('package.json', './.layer/nodejs/package.json');
+fs.copyFileSync('package-lock.json', './.layer/nodejs/package-lock.json');
 execSync('npm install --omit=dev --prefix ./.layer/nodejs', { stdio: 'inherit' });
 console.log('\n✨  Layer prepped!');
 
